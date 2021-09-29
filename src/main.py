a64e7cade8bd32a44bac6c6afb18881f63c94374
alexabram <alexabram@yahoo.com>
2021-09-29 11:47:28 -0700

    Added date parser

--- src/main.py
+++ src/main.py
@@ -1,17 +1,57 @@
 import sys # for use w/ cmdline arguments
 import variables # local file
+import datetime
 
-def readFile(filename):
+def parseFile(filename):
   with open(filename, "r") as fd:
-    fileText = fd.read().lower()
-    print(fileText);
+    # date looks like "Started on: September 2, 2021"
+    # read until fd is at 'S' after ": "
+    while(True):
+      char = fd.read(1);
+      if char == ':':
+        char = fd.read(1);
+        break
+    date_string = ""
+    char = ''
+    # read entire Month D, YYYY into a string
+    while(char != '\n'):
+      date_string += char
+      char = fd.read(1)
+    print(date_string)
+    week_number = parseDate(date_string)
+    print(week_number)
+
+
+def parseDate(date_string):
+  # parse string for MM, DD, YY
+  # use datetime to get week #
+  split_date = date_string.split(' ');
+  month_number = getMonthNumber(split_date[0])
+  year_number = int(split_date[2])
+  day_number = int(split_date[1][:-1])
+  return datetime.date(year_number, month_number, day_number).isocalendar()[1]
+
+def getMonthNumber(month_string):
+  return{
+    "January": 1,
+    "February": 2,
+    "March": 3,
+    "April": 4,
+    "May": 5,
+    "June": 6,
+    "July": 7,
+    "August": 8,
+    "September": 9,
+    "October": 10,
+    "November": 11,
+    "December": 12,
+  }[month_string]
+
 
 if __name__ == "__main__":
   # if len(sys.argv) != 2:
   #   sys.exit(2)
   # else:
-  #   readFile(sys.argv[1])
+  #   parseFile(sys.argv[1])
 
-  print(variables.muscleGroup["chest"])
-  for exercise in variables.muscleGroup["shoulders"]["exercises"]:
-    print(exercise);
+  parseFile("data/sep2.txt")
--- src/variables.py
+++ src/variables.py
@@ -1,63 +1,61 @@
-muscleGroup ={
-  "chest":{
-    "exercises":[
-      "incline db press",
-      "flat db press",
-      "cable fly",
-      "bench press",
-      "incline pause bench press",
-      "pec dec fly",
-    ]
-  },
-  "back":{
-    "exercises":[
-      "deadlift",
-      "barbell row",
-      "lat pulldown",
-      "machine row",
-      "lower back",
-      "neutral grip pulldown",
-      "chest supported row",
-      "cable row",
-    ]
-  },
-  "legs":{
-    "exercises":[
-      "squat",
-      "leg press",
-      "leg extension",
-      "hamstring curl",
-      "standing calve raise",
-      "seated calve raise",
-      "db bulgarian split squat",
-      "romanian deadlift",
-    ]
-  },
-  "shoulders":{
-    "exercises":[
-      "dumbbell shoulder press",
-      "machine lateral raise",
-      "lateral raise",
-      "cable reverse fly",
-      "wide-grip face pull",
-      "standing press",
-      "smith machine shoulder press",
-    ]
-  },
-  "biceps":{
-    "exercises":[
-      "barbell  curl",
-      "preacher curl",
-      "hammer curl",
-      "conc curl"
-    ]
-  },
-  "triceps":{
-    "exercises":[
-      "tricep pushdown",
-      "french press",
-      "cable overhead",
-      "db skull crusher"
-    ]
-  }
+exercises ={
+  "chest":[
+    "incline db press",
+    "flat db press",
+    "cable fly",
+    "bench press",
+    "incline pause bench press",
+    "pec dec fly",
+  ],
+  "back":[
+    "deadlift",
+    "barbell row",
+    "lat pulldown",
+    "machine row",
+    "lower back",
+    "neutral grip pulldown",
+    "chest supported row",
+    "cable row",
+  ],
+  "legs":[
+    "squat",
+    "leg press",
+    "leg extension",
+    "hamstring curl",
+    "lying hamstring curl",
+    "standing calve raise",
+    "seated calve raise",
+    "db bulgarian split squat",
+    "romanian deadlift",
+  ],
+  "shoulders":[
+    "dumbbell shoulder press",
+    "machine lateral raise",
+    "lateral raise",
+    "cable reverse fly",
+    "wide-grip face pull",
+    "standing press",
+    "smith machine shoulder press",
+  ],
+  "biceps":[
+    "barbell  curl",
+    "preacher curl",
+    "hammer curl",
+    "conc curl"
+  ],
+  "triceps":[
+    "tricep pushdown",
+    "french press",
+    "cable overhead",
+    "db skull crusher",
+    "cable overhead"
+  ],
 }
+
+
+
+
+
+
+
+

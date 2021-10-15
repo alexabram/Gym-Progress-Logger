10ab394dfba912a563b449f531bffdfe1e71e0e9
alexabram <alexabram@yahoo.com>
2021-10-15 13:59:12 -0700

    add year and exercise dropdown menus

--- progress-keeper/src/app/view-data/view-data.component.ts
+++ progress-keeper/src/app/view-data/view-data.component.ts
@@ -8,13 +8,46 @@ import ProgressLogJSON from '../../../../data/exports/ProgressLog.json'
 })
 export class ViewDataComponent implements OnInit {
 
-  progressLog = ProgressLogJSON
+  progressLog:any = ProgressLogJSON; // {string: {string: {string: [int]}}}
+  allYears:string[] = [];
+  allExerciseNames:string[] = [];
+  allWeeks:string[] = [];
+
+  testing:string[] = ["one", "two", "three", "four"];
+  
+  yearSelected:boolean = false;
+  selectedYear:string = "";
+  selectedExercise:string = "";
 
   constructor() { }
 
   ngOnInit(): void {
-    console.log(this.progressLog)
-    console.log(this.progressLog["2021"])
+    // populate allYears
+    this.allYears = Object.keys(this.progressLog);
+
+    // for(let key in this.progressLog){
+    //   console.log(this.progressLog[key])
+    //   console.log(key)
+    //   var allWeeks = Object.keys(this.progressLog[key])
+    //   console.log(allWeeks)
+    // }
+    // // this.allYears = Object.keys(this.progressLog)
+    // this.populateAllExerciseNames()
   }
 
+  populateAllExercises(year:string){
+    this.allWeeks = Object.keys(this.progressLog[year])
+    for(let i = 0; i < this.allWeeks.length; ++i){
+      let week = this.allWeeks[i]
+      // this.allExerciseNames.push(Object.keys(this.progressLog[year][week]))
+    }
+  }
+
+  // Call this upon click on select year
+  yearSelect(year:string){
+    // populate allExerciseNames and list them
+    this.selectedYear = year;
+    this.yearSelected = true;
+    this.populateAllExercises(this.selectedYear);
+  }
 }

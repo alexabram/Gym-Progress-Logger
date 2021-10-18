7081760232daaa6f57fe21cf6eec7d7851396fe0
alexabram <alexabram@yahoo.com>
2021-10-18 13:28:34 -0700

    add component binding to populate table

--- progress-keeper/src/app/load-data/load-data.component.ts
+++ progress-keeper/src/app/load-data/load-data.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import ProgressLogJSON from '../../../../data/exports/ProgressLog.json'
 
 @Component({
   selector: 'app-load-data',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./load-data.component.scss']
 })
 export class LoadDataComponent implements OnInit {
+  @Input() exercise:string;
+  @Input() year:string;
+  progressLog:any = ProgressLogJSON; // {yr:string: {wk:string: {exercise:string: [int]}}}
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.exercise)
+    console.log(this.year)
   }
 
 }

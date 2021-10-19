5b254c976bdbc8a9bb90e5ad22536dde5569bf8f
alexabram <alexabram@yahoo.com>
2021-10-19 12:53:04 -0700

    add data filling for scatter chart

--- progress-keeper/src/app/scatter-chart/scatter-chart.component.ts
+++ progress-keeper/src/app/scatter-chart/scatter-chart.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import ProgressLogJSON from '../../../../data/exports/ProgressLog.json'
 
 @Component({
   selector: 'app-scatter-chart',
@@ -6,10 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scatter-chart.component.scss']
 })
 export class ScatterChartComponent implements OnInit {
+  @Input() exercise:string;
+  @Input() year:string;
+  progressLog:any = ProgressLogJSON; // {yr:string: {wk:string: {exercise:string: [int]}}}
+
+  allWeeks:string[] = [];
+  data:{x: number, y:number}[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.allWeeks = Object.keys(this.progressLog[this.year]);
+    this.sortWeights()
+    this.fillData()
   }
 
+  sortWeights():void{
+    for(let i = 0; i < this.allWeeks.length; ++i){
+      this.progressLog[this.year][this.allWeeks[i]][this.exercise].sort()
+    }
+  }
+
+  fillData():void{
+    for(let i = 0; i < this.allWeeks.length; ++i){
+      var week:number = Number(this.allWeeks[i])
+      var weights = this.progressLog[this.year][week][this.exercise]
+      for(let j = 0; j < weights.length; ++j){
+        var weight:number = weights[j]
+        this.data.push(this.createPoint(week, weight));
+      }
+    }
+  }
+
+  createPoint(week:number, weight:number):Point{
+    var p:Point = {
+      x: week,
+      y: weight,
+    }
+    return p;
+  }
+
+}
+
+interface Point {
+  x:number,
+  y:number
 }

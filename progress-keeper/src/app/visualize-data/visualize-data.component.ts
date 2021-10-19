71d1a59bee7845009069ac888490379540631d9a
alexabram <alexabram@yahoo.com>
2021-10-19 12:03:19 -0700

    add UI to select year and exercise.

--- progress-keeper/src/app/visualize-data/visualize-data.component.ts
+++ progress-keeper/src/app/visualize-data/visualize-data.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+import ProgressLogJSON from '../../../../data/exports/ProgressLog.json'
 
 @Component({
   selector: 'app-visualize-data',
@@ -6,10 +8,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./visualize-data.component.scss']
 })
 export class VisualizeDataComponent implements OnInit {
+  progressLog:any = ProgressLogJSON; // {yr:string: {wk:string: {exercise:string: [int]}}}
+  allYears:string[] = [];
+  // allWeeks:string[] = [];
+  allExerciseNames:string[] = [];
+  yearSelected:boolean = false;
+  selectedYear:string = "";
+  exerciseSelected:boolean = false;
+  selectedExercise:string = "";
 
   constructor() { }
 
   ngOnInit(): void {
+    this.allYears = Object.keys(this.progressLog);
+  }
+
+  // Populate allExerciseNames
+  getAllExerciseNames(year:string){
+    // Using class variable allWeeks:string[]
+    // this.allWeeks = Object.keys(this.progressLog[year]);
+    // for(let i = 0; i < this.allWeeks.length; ++i){
+    //   this.allExerciseNames = Object.keys(this.progressLog[year][this.allWeeks[i]]);
+    // }
+    var allWeeks:string[] = Object.keys(this.progressLog[year]);
+    for(let i = 0; i < allWeeks.length; ++i){
+      this.allExerciseNames = Object.keys(this.progressLog[year][allWeeks[i]]);
+    }
+  }
+
+  // User selected year
+  yearSelect(year:string){
+    this.selectedYear = year;
+    this.yearSelected = true;
+    this.getAllExerciseNames(this.selectedYear);
+  }
+
+  // User selected exericse
+  exerciseSelect(exercise:string){
+    this.exerciseSelected = true;
+    this.selectedExercise = exercise;
   }
 
 }
